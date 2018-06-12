6a8becc051d655a70b2ea05856df03078854685a
motokimura <motokimura1@gmail.com>
2018-06-12 23:48:22 +0900

    Replace indentations with tabs

--- create_poly_mask.py
+++ create_poly_mask.py
@@ -12,44 +12,44 @@ import os
 
 
 def create_poly_mask(rasterSrc, vectorSrc, npDistFileName='', 
-                            noDataValue=0, burn_values=1):
+							noDataValue=0, burn_values=1):
 
-    '''
-    Create polygon mask for rasterSrc,
-    Similar to labeltools/createNPPixArray() in spacenet utilities
-    '''
-    
-    ## open source vector file that truth data
-    source_ds = ogr.Open(vectorSrc)
-    source_layer = source_ds.GetLayer()
+	'''
+	Create polygon mask for rasterSrc,
+	Similar to labeltools/createNPPixArray() in spacenet utilities
+	'''
+	
+	## open source vector file that truth data
+	source_ds = ogr.Open(vectorSrc)
+	source_layer = source_ds.GetLayer()
 
-    ## extract data from src Raster File to be emulated
-    ## open raster file that is to be emulated
-    srcRas_ds = gdal.Open(rasterSrc)
-    cols = srcRas_ds.RasterXSize
-    rows = srcRas_ds.RasterYSize
+	## extract data from src Raster File to be emulated
+	## open raster file that is to be emulated
+	srcRas_ds = gdal.Open(rasterSrc)
+	cols = srcRas_ds.RasterXSize
+	rows = srcRas_ds.RasterYSize
 
-    if npDistFileName == '':
-        dstPath = ".tmp.tiff"
-    else:
-        dstPath = npDistFileName
+	if npDistFileName == '':
+		dstPath = ".tmp.tiff"
+	else:
+		dstPath = npDistFileName
 
-    ## create First raster memory layer, units are pixels
-    # Change output to geotiff instead of memory 
-    memdrv = gdal.GetDriverByName('GTiff') 
-    dst_ds = memdrv.Create(dstPath, cols, rows, 1, gdal.GDT_Byte, 
-                           options=['COMPRESS=LZW'])
-    dst_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    dst_ds.SetProjection(srcRas_ds.GetProjection())
-    band = dst_ds.GetRasterBand(1)
-    band.SetNoDataValue(noDataValue)    
-    gdal.RasterizeLayer(dst_ds, [1], source_layer, burn_values=[burn_values])
-    dst_ds = 0
+	## create First raster memory layer, units are pixels
+	# Change output to geotiff instead of memory 
+	memdrv = gdal.GetDriverByName('GTiff') 
+	dst_ds = memdrv.Create(dstPath, cols, rows, 1, gdal.GDT_Byte, 
+						   options=['COMPRESS=LZW'])
+	dst_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
+	dst_ds.SetProjection(srcRas_ds.GetProjection())
+	band = dst_ds.GetRasterBand(1)
+	band.SetNoDataValue(noDataValue)    
+	gdal.RasterizeLayer(dst_ds, [1], source_layer, burn_values=[burn_values])
+	dst_ds = 0
 
-    mask_image = Image.open(dstPath)
-    mask_image = np.array(mask_image)
+	mask_image = Image.open(dstPath)
+	mask_image = np.array(mask_image)
 
-    if npDistFileName == '':
-        os.remove(dstPath)
-        
-    return mask_image
+	if npDistFileName == '':
+		os.remove(dstPath)
+		
+	return mask_image
